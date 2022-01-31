4fff96079a21ee44eb0ab5cb7077386576b94caf
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-01-31 15:26:03 -0600

    Add external.js, external_js.html, and inline_js.html files

---
+++ JS/external.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log("Hello from external JavaScript!");
+
+alert("Welcome to my website!");
+
+var userFavColor = prompt("What is your favorite color?");
+alert(userFavColor + " is my favorite color too!");
