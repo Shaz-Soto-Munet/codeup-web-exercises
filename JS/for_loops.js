cad50184aac35befdff36ae56ff8d58132ae78f1
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-02-08 10:33:57 -0600

    update the output of exercise 2 to show not only the result of the multiplication but the equation as well

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -1,12 +1,11 @@
 "use strict";
 
-//exercise 2
+// exercise 2
 // function showMultiplicationTable(num){
 // 	for (var i=1; i<=10; i++){
-// 		console.log(num * i);
+// 		console.log(num + " x " + i + " = " + (num * i));
 // 	}
 // }
-
 // showMultiplicationTable(7);
 
 //exercise 3
@@ -19,11 +18,9 @@
 // }
 
 //exercise 4: unfinished
-
-
-for (var i="1"; i.length<10; i+="1"){
-	console.log(i*i.length);
-}
+// for (var i="1"; i.length<10; i+="1"){
+// 	console.log(i*i.length);
+// }
 
 //exercise 5
 // for (var i=100; i>0; i=i-5){
