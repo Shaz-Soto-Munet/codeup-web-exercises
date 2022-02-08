fac3ed7c6d2bb78d87236d01318c6511ee2eae9d
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-02-08 15:00:47 -0600

    add array lecture files

---
+++ js/arrays_lecture.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var groceryList = ["bread", "eggs", "butter", "milk"];
+// console.log("I have to buy " + groceryList[3] + ".");
+var people = ["Pat", "John", "Kate", "Orion"];
+// console.log(people[0], people[3]);
+// console.log(people[3] + " is my friend.");
+// console.log(people[2] + " is also my friend.")
+// console.log("There are " + groceryList.length + " names in our array.")
+
+for (var i=0; groceryList.length>i; i++){
+	console.log("I have to buy " + groceryList[i] + ".");
+}
+
+// Given an array
+var cities = ["San Antonio", "Austin", "Dallas", "Houston"];
+// First, create a for loop that generates the output:
+//     ```
+// San Antonio
+// Austin
+// Dallas
+// Houston
+// ```
+
+for (var i=0; cities.length>i; i++){
+	// console.log(cities[i])
+}
+
+// Then elaborate on the loop output to generate output like:
+//     ```
+// Student number 1 lives in San Antonio
+// Student number 2 lives in Austin
+// Student number 3 lives in Dallas
+// Student number 4 lives in Houston
+// ```
+
+for (var i=0; cities.length>i; i++){
+	console.log("Student number " + (parseInt(i)+1) + " lives in " + cities[i]);
+}
+
+// Remember the key concept that you can use the iterator variable (i) to access array elements in a loop.
+
+var prices = [32.99, 21.99, 6.99, 4.99, 12.99, 8.98, 5.99];
+prices.forEach(function(price, index){
+	console.log("Item number " + (index+1) + ": " + price);
+});
+
+
+
+
+
+
+
