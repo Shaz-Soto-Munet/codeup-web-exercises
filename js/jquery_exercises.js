0889e4f2a2aad36c784720cc0cd92116e5533041
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-03-08 14:17:14 -0600

    Complete mouse events jQuery exercise.

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,18 +1,35 @@
 $(document).ready(function() {
 
-	"use strict";
 
-	$(".codeup").css("border", "1px solid red");
-	$("li").css("font-size", "20px");
-	$("h1, p, li").css("background-color", "yellow");
-	// alert($("h1")[0].innerText);
-	$("div:nth-of-type(1)").css({
-		"background-color": "blue",
-		"font-weight": "bold"
+
+	// "use strict";
+	//
+	// $(".codeup").css("border", "1px solid red");
+	// $("li").css("font-size", "20px");
+	// $("h1, p, li").css("background-color", "yellow");
+	// // alert($("h1")[0].innerText);
+	// $("div:nth-of-type(1)").css({
+	// 	"background-color": "blue",
+	// 	"font-weight": "bold"
+	// });
+	// console.log($("div")[1].innerText);
+	// console.log($("div"));
+	//
+
+//Mouse Events:
+
+	$("h1").click(function(){
+		$(this).css("background-color", "blue")
 	});
-	console.log($("div")[1].innerText);
-	console.log($("div"));
 
+	$("p").dblclick(function(){
+		$(this).css("font-size", "18px");
+	});
 
+	$('li').hover(function(){
+		$(this).css("color", "red");
+	}, function(){
+		$(this).css("color", "black");
+	});
 
 });
