c2263002bef40ee6a7240ae04ac657f7a4de0b19
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-03-15 09:50:43 -0500

    Add search box funcitonality to mapbox exercise.

--- js/js_warmups.js
+++ js/js_warmups.js
@@ -490,3 +490,44 @@ function countNegatives(array){
 
 //-------------------------------------------------------------------
 
+// Create a function, createFirstNamesArray, that takes in an array of user objects and returns an array of first name strings. Assume that each user object has a property called 'firstName'.
+//
+// 	Example...
+
+function createFirstNamesArray(array){
+	var firstNames = [];
+	for (var i=0; i<array.length; i++){
+		firstNames.push(array[i].firstName);
+	}
+	return firstNames;
+}
+
+var users = [
+	{
+		firstName: 'Bob',
+		lastName: 'Smith',
+		email: 'bob@email.com'
+	},
+	{
+		firstName: 'Sally',
+		lastName: 'Smith',
+		email: 'sally@email.com'
+	},
+	{
+		firstName: 'Fred',
+		lastName: 'Smith',
+		email: 'fred@email.com'
+	},
+	{
+		firstName: 'Cathy',
+		lastName: 'Smith',
+		email: 'cathy@email.com'
+	},
+	{
+		firstName: 'Joe',
+		lastName: 'Smith',
+		email: 'joe@email.com'
+	},
+];
+
+console.log(createFirstNamesArray(users));; // returns... ['Bob', 'Sally', 'Fred', 'Cathy', 'Joe']
