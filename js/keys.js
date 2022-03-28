2af651073c0b6ad2facd79c2832b3ebf7637e016
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-03-28 09:56:17 -0500

    Delete keys info

--- js/keys.js
+++
@@ -1,5 +0,0 @@
-const MAPBOX_API_TOKEN  = "pk.eyJ1Ijoic2hhei1zb3RvLW11bmV0IiwiYSI6ImNsMG1xM2xlYjE5MmszY3JrM3IxMzVhaDgifQ.hvEU0PNcIjt4QHrxFYXRzw"
-
-const OPEN_WEATHER_TOKEN = "cfd2644d22070aaab3e77f6e62758ed1"
-
-const GITHUB_TOKEN = "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"
